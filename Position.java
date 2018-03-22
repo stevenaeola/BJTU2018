0917af28c6928ed65542a1c25030b399df69ffb3
danieltesfai <37664435+danieltesfai@users.noreply.github.com>
2018-03-22 23:52:52 +0800

    Update Position.java
    added getters and setters methods to the class.

--- Position.java
+++ Position.java
@@ -22,10 +22,21 @@ public class Position
         this.y = y;
     }
 
+    public void setX(int x)
+    {
+        this.x=x;
+    }
     public int getX(){
         return x;
     }
-
+    public void setY(int y)
+    {
+        this.y=y;
+    }
+    public int getY(){
+        return y;
+    }
+ 
     public int setToFive(){
         x=5;
         return ++x;
