d99ec0e215531aea634a48cbecfed27556758fee
dcs0spb <s.p.bradley@durham.ac.uk>
2018-03-22 09:34:43 +0800

    Initial commit

---
+++ Position.java
@@ -0,0 +1,39 @@
+
+public class Position
+{
+    private int x;
+    private int y;
+
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Position(){
+        x = 5;
+        y = 7;
+    }
+
+    public void Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int setToFive(){
+        x=5;
+        return ++x;
+    }
+
+    public void print(){
+        System.out.println("x is " + x);
+    }
+
+    public boolean xIsSmall(){
+        return !(x>3 || y/0>100);
+        
+    }
+}
